eb7565538292266c8a6c3b67b0d5dc6622f78356
juan <jpuerto96@hotmail.com>
2026-04-12 10:33:50 -0400

    feat: get robot/gcs connected across network

--- gcs/ros_ws/src/routine_executor/routine_executor/step_config.py
+++ gcs/ros_ws/src/routine_executor/routine_executor/step_config.py
@@ -6,53 +6,53 @@ from behavior_tree_msgs.msg import LabMachineCommand, ManipulationCommand
 STEPS = {
     'pick_base': {
         'msg_type': 'manipulation',
-        'publish_topic': 'manipulation_command',
+        'publish_topic': 'behavior/manipulation_command',
         'make_msg': lambda: ManipulationCommand(
             type='pick_base',
             object_type='well_plate',
             target_machine='',
         ),
-        'watch_topic': 'pick_base_object_status',
+        'watch_topic': 'behavior/pick_base_object_status',
     },
     'place': {
         'msg_type': 'manipulation',
-        'publish_topic': 'manipulation_command',
+        'publish_topic': 'behavior/manipulation_command',
         'make_msg': lambda: ManipulationCommand(
             type='place',
             object_type='well_plate',
             target_machine='ot2',
         ),
-        'watch_topic': 'place_object_status',
+        'watch_topic': 'behavior/place_object_status',
     },
     'pick_up': {
         'msg_type': 'manipulation',
-        'publish_topic': 'manipulation_command',
+        'publish_topic': 'behavior/manipulation_command',
         'make_msg': lambda: ManipulationCommand(
             type='pick_up',
             object_type='well_plate',
             target_machine='',
         ),
-        'watch_topic': 'pick_up_object_status',
+        'watch_topic': 'behavior/pick_up_object_status',
     },
     'ot2': {
         'msg_type': 'lab_machine',
-        'publish_topic': 'lab_machine_command',
+        'publish_topic': 'behavior/lab_machine_command',
         'make_msg': lambda: LabMachineCommand(
             device='ot2',
             action='protocol',
             parameters_json='{}',
         ),
-        'watch_topic': 'execute_ot2_protocol_status',
+        'watch_topic': 'behavior/execute_ot2_protocol_status',
     },
     'shaker': {
         'msg_type': 'lab_machine',
-        'publish_topic': 'lab_machine_command',
+        'publish_topic': 'behavior/lab_machine_command',
         'make_msg': lambda: LabMachineCommand(
             device='shaker',
             action='protocol',
             parameters_json='{}',
         ),
-        'watch_topic': 'execute_shaker_protocol_status',
+        'watch_topic': 'behavior/execute_shaker_protocol_status',
     },
 }
 
